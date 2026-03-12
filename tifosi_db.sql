8854bf70a3f78ddde2125a33efa5170707e60db9
Kevin Marlin <kevin.marlin@gmail.com>
2026-03-12 19:35:52 +0100

    add tifosi database

---
+++ tifosi_db.sql
@@ -0,0 +1,5 @@
+-- Création de la base de données tifosi
+CREATE DATABASE  IF NOT EXISTS `tifosi` DEFAULT CHARACTER SET  utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Sélection de la base de donnée
+USE `tifosi`;
